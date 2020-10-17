55221bbf82f55cdf4971263455048d1e266fdb37
maaiked <67857316+maaiked@users.noreply.github.com>
2020-10-17 14:36:35 +0200

    Signature toegevoegd

--- src/Signature.js
+++ src/Signature.js
@@ -1,3 +1,12 @@
-module.exports = () => {
+const nacl = require('libsodium-wrappers');
 
+module.exports = async () => {
+    await nacl.ready;
+
+    const key = nacl.crypto_sign_keypair();
+
+    return Object.freeze({
+        verifyingKey: key.publicKey,
+        sign: (msg) => nacl.crypto_sign(msg, key.privateKey)
+    })
 };
